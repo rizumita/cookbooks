87c64320f899e5f9de8584d881c6d79417f12e25
Ryoichi Izumita <r.izumita@caph.jp>
2012-02-18 10:52:28 +0900

    fuelphpレシピの修正

--- fuelphp/recipes/deploy.rb
+++ fuelphp/recipes/deploy.rb
@@ -1,4 +1,9 @@
 node[:deploy].each do |application, deploy|
   `cd #{deploy[:deploy_to]}/current && php oil refine install`
-  'cd #{deploy[:deploy_to]}/current && php oil refine migrate`
+  `cd #{deploy[:deploy_to]}/current && php oil refine migrate`
+end
+
+node[:deploy].each do |application, deploy|
+  template "#{deploy[:deploy_to]/fuel/app/bootstrap.php" do 
+  end
 end
