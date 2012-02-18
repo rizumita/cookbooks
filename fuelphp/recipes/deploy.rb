e5e212d08179744e4b3b32fe4ec25c1d0e0d4f87
Ryoichi Izumita <r.izumita@caph.jp>
2012-02-19 04:32:06 +0900

    fuelphpのtemplatesにdefaultディレクトリを作成

--- fuelphp/recipes/deploy.rb
+++ fuelphp/recipes/deploy.rb
@@ -10,13 +10,21 @@ node[:deploy].each do |application, deploy|
   end
 
   template "#{deploy[:current_path]}/fuel/app/bootstrap.php" do
-    #source "bootstrap.php.erb"
+    source "bootstrap.php.erb"
     #variables(:hosts => node[:scalarium][:roles][:search][:instances].keys)
     group deploy[:group]
     owner deploy[:user]
     #only_if { File.exists?("#{deploy[:current_path]}/fuel/app/") }
   end
 
+  #template "#{deploy[:current_path]}/fuel/app/config/production/db.php" do
+  #  source "db.php.erb"
+  #  #variables(:hosts => node[:scalarium][:roles][:search][:instances].keys)
+  #  group deploy[:group]
+  #  owner deploy[:user]
+  #  #only_if { File.exists?("#{deploy[:current_path]}/fuel/app/") }
+  #end
+  
   execute "oil refine migrate" do
     cwd deploy[:current_path]
     user deploy[:user]
