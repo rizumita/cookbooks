a1f8f6b7638ddc2b8286718ab52c3e3947aca30d
Ryoichi Izumita <r.izumita@caph.jp>
2012-02-18 11:47:45 +0900

    fuelphp::deployの修正

--- fuelphp/recipes/deploy.rb
+++ fuelphp/recipes/deploy.rb
@@ -1,6 +1,28 @@
 node[:deploy].each do |application, deploy|
-  execute "cd #{deploy[:deploy_to]}/current && root php oil refine install"
-  execute "cd #{deploy[:deploy_to]}/current && root php oil refine migrate"
+  `cd #{deploy[:deploy_to]}/current && sudo php oil refine install`
+  `cd #{deploy[:deploy_to]}/current && sudo php oil refine migrate`
+
+  #execute "oil refine install" do
+  #  cwd deploy[:current_path]
+  #  command "sudo php oil refine install"
+  #  action :nothing
+  #end
+  #
+  #execute "oil refine migrate" do
+  #  cwd deploy[:current_path]
+  #  command "sudo php oil refine migrate"
+  #  action :nothing
+  #end
+
+  #template "#{deploy[:deploy_to]}/shared/config/search.yml" do
+  #  source "search.yml.erb"
+  #  variables(:hosts => node[:scalarium][:roles][:search][:instances].keys)
+  #  mode "0660"
+  #  group deploy[:group]
+  #  owner deploy[:user]
+  #  notifies :run, resources(:execute => "restart Rails app #{application}")
+  #  only_if { File.exists?("#{deploy[:deploy_to]}/shared/config/") }
+  #end
 end
 
 #node[:deploy].each do |application, deploy|
