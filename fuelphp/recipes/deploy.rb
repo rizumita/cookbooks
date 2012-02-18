b91275f06f01b206f93bfd54c2bcb40ef4c50766
Ryoichi Izumita <r.izumita@caph.jp>
2012-02-18 18:44:05 +0900

    fuelphp::deployの修正

--- fuelphp/recipes/deploy.rb
+++ fuelphp/recipes/deploy.rb
@@ -1,33 +1,26 @@
+include_recipe "deploy::default"
+
 node[:deploy].each do |application, deploy|
-  puts "deploy_to: " + deploy[:deploy_to]
-  puts "current_path" + deploy[:current_path]
-  `cd #{deploy[:deploy_to]} && sudo php oil refine install`
-  `cd #{deploy[:current_path]} && sudo php oil refine migrate`
+# deploy[:deploy_to]
+  
+  execute "oil refine install" do
+    cwd deploy[:current_path]
+    user deploy[:user]
+    command "php oil refine install"
+  end
 
-  #execute "oil refine install" do
-  #  cwd deploy[:current_path]
-  #  command "sudo php oil refine install"
-  #  action :nothing
-  #end
-  #
-  #execute "oil refine migrate" do
-  #  cwd deploy[:current_path]
-  #  command "sudo php oil refine migrate"
-  #  action :nothing
-  #end
+  template "#{deploy[:current_path]}/fuel/app/bootstrap.php" do
+    source "bootstrap.php.erb"
+    #variables(:hosts => node[:scalarium][:roles][:search][:instances].keys)
+    group deploy[:group]
+    owner deploy[:user]
+    #only_if { File.exists?("#{deploy[:current_path]}/fuel/app/") }
+  end
 
-  #template "#{deploy[:deploy_to]}/shared/config/search.yml" do
-  #  source "search.yml.erb"
-  #  variables(:hosts => node[:scalarium][:roles][:search][:instances].keys)
-  #  mode "0660"
-  #  group deploy[:group]
-  #  owner deploy[:user]
-  #  notifies :run, resources(:execute => "restart Rails app #{application}")
-  #  only_if { File.exists?("#{deploy[:deploy_to]}/shared/config/") }
-  #end
+  execute "oil refine migrate" do
+    cwd deploy[:current_path]
+    user deploy[:user]
+    command "php oil refine migrate"
+  end
+
 end
-
-#node[:deploy].each do |application, deploy|
-#  template "#{deploy[:deploy_to]/fuel/app/bootstrap.php" do 
-#  end
-#end
